cd968fae030f6d470899aa6c40e7c13cd75b03bd
ryo5113 <145419239+ryo5113@users.noreply.github.com>
2025-12-28 21:35:50 +0900

    12/28　進捗
    YOLOのモデル作成
    精度は0.95といい感じな気がする

--- Sound/soundTrain_yolo.py
+++ Sound/soundTrain_yolo.py
@@ -6,7 +6,7 @@ model = YOLO("yolov8n-cls.pt")
 # data には、train/ と val/ を含む「データセットのルート」を渡す
 # 例: ML_spectrogram_dataset_yolo_cls
 results = model.train(
-    data="spectrogram_normmax_by_label_yolo_cls",  # データセットのルートフォルダ
+    data="spectrogram02_normmax_by_label_yolo_cls",  # データセットのルートフォルダ
     epochs=100,
-    imgsz=2000,   # 必須項目なので指定例（変更可）
+    imgsz=1000,   # 必須項目なので指定例（変更可）
 )
