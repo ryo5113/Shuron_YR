606eb3e26d734f5d95010a9476cf931657f42e63
ryo5113 <yano.ryo.y0@s.gifu-u.ac.jp>
2025-12-30 13:14:20 +0900

    12/30　進捗①
    YOLOのモデル構築を実施
    （使わないかもしれないけど）

--- Sound/soundTrain_yolo.py
+++ Sound/soundTrain_yolo.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 
-# 例：YOLO8の分類用事前学習モデル
+# 例：YOLOv8の分類用事前学習モデル
 model = YOLO("yolov8n-cls.pt")
 
 # data には、train/ と val/ を含む「データセットのルート」を渡す
 # 例: ML_spectrogram_dataset_yolo_cls
 results = model.train(
-    data="spectrogram02_normmax_by_label_yolo_cls",  # データセットのルートフォルダ
+    data="spectrogram03_normmax_by_label_yolo_cls",  # データセットのルートフォルダ
     epochs=100,
-    imgsz=1000,   # 必須項目なので指定例（変更可）
+    imgsz=1024,   # 必須項目なので指定例（変更可）
 )
